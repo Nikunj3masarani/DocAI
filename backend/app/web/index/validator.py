05fffbadbc8f6a0690ef7c97ec1b4a4f34a47788
Nikunj Masrani <nikunj.masrani@smartsensesolutions.com>
2024-04-16 10:47:24 +0530

    - Update index api updated

--- backend/app/web/index/validator.py
+++ backend/app/web/index/validator.py
@@ -35,18 +35,6 @@ class UpdateIndex(BaseModel):
     prompt_uuid: Optional[str]
     model: Optional[str]
 
-    @validator('title')
-    def check_no_spaces(cls, v):
-        regex = re.compile(r'^[a-zA-Z0-9_]+$')
-        match = regex.match(v)
-        if len(v) < 4:
-            raise CustomException("title must contain at-least 4 character")
-        if not bool(match):
-            raise CustomException("only alphanumeric and _ allowed")
-        if ' ' in v:
-            raise CustomException('Spaces are not allowed')
-        return v
-
 
 class IndexList(BaseModel):
     search: Optional[str] = None
