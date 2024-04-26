15312860f54cc21695bbf0abefc2a8106c1172f4
Nikunj Masrani <nikunj.masrani@smartsensesolutions.com>
2024-04-26 18:49:52 +0530

    - Email API changes

--- backend/app/web/users/email_service.py
+++ backend/app/web/users/email_service.py
@@ -17,7 +17,7 @@ class Users:
             from_email = settings.from_email
             name = str(from_email).split("@")[0]
             
-            url = str(os.getenv("FRONTEND_SERVICE",""))+"/set-password?user_uuid="+ data.get("user_uuid")+"&token="+data.get("token")+"&action="+data.get("action")
+            url = str(os.getenv("FRONTEND_SERVICE",""))+"/auth/set-user-details?user_uuid="+ data.get("user_uuid")+"&token="+data.get("token")+"&action="+data.get("action")
             
             content = INVITE_USER.format(name=name,url=url)
             message = Mail(from_email, data.get("email"), INVITE_USER_EMAIL_SUBJECT,html_content= content)
