e93849a9149123de8228c4e08895424a23e9b0f1
Nikunj Masrani <nikunj.masrani@smartsensesolutions.com>
2024-04-29 12:06:56 +0530

    - Email API changes

--- backend/app/web/index/email_service.py
+++ backend/app/web/index/email_service.py
@@ -10,20 +10,14 @@ class Users:
     def __init__(self):
         api_key = settings.email_api_key
         self.send_grid = SendGridAPIClient(api_key)
-        
 
     def invite_user_for_index(self, data):
-       
         from_email = settings.from_email
         name = str(from_email).split("@")[0]
-        
-        url = str(os.getenv("FRONTEND_SERVICE",""))+"/index-invite?user_uuid="+ data.get("user_uuid")+"&token="+data.get("token")+"&status="+data.get("status")+"&index_uuid="+data.get("index_uuid")
-        
-        content = INDEX_INVITE_USER.format(name=name,url=url)
-        message = Mail(from_email, data.get("email"), INVITE_INDEX_USER_EMAIL_SUBJECT,html_content= content)
-        
-        response = self.send_grid.send(message)
-        
-        print(response.status_code, response.body, response.headers)
- 
 
+        url = f'''{str(os.getenv("FRONTEND_SERVICE", ""))}/index-invite?user_uuid={data.get("user_uuid")}&token={data.get("token")}&status={data.get("status")}&index_uuid={data.get("index_uuid")}'''
+
+        content = INDEX_INVITE_USER.format(name=name, url=url)
+        message = Mail(from_email, data.get("email"), INVITE_INDEX_USER_EMAIL_SUBJECT, html_content=content)
+
+        response = self.send_grid.send(message)
