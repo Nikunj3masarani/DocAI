ae84cbca22f306008ec737cdd28092983cdccf80
Nikunj Masrani <nikunj.masrani@smartsensesolutions.com>
2024-05-14 09:53:19 +0530

    - Lida Streamlit APP integration

--- structured_data_poc/main.py
+++ structured_data_poc/main.py
@@ -9,39 +9,22 @@ import pandas as pd
 os.makedirs("data", exist_ok=True)
 
 st.set_page_config(
-    page_title="LIDA: Automatic Generation of Visualizations and Infographics",
+    page_title="DocAI - LIDA: Automatic Generation of Visualizations",
     page_icon="📊",
 )
 
-st.write("# LIDA: Automatic Generation of Visualizations and Infographics using Large Language Models 📊")
+st.write("# DocAI - LIDA: Automatic Generation of Visualizations 📊")
 
-st.sidebar.write("## Setup")
 
 # Step 1 - Get OpenAI API key
 openai_key = os.getenv("OPENAI_API_KEY")
 
-if not openai_key:
-    openai_key = st.sidebar.text_input("Enter OpenAI API key:")
-    if openai_key:
-        display_key = openai_key[:2] + "*" * (len(openai_key) - 5) + openai_key[-3:]
-        st.sidebar.write(f"Current key: {display_key}")
-    else:
-        st.sidebar.write("Please enter OpenAI API key.")
-else:
-    display_key = openai_key[:2] + "*" * (len(openai_key) - 5) + openai_key[-3:]
-    st.sidebar.write(f"OpenAI API key loaded from environment variable: {display_key}")
-
 st.markdown(
     """
-    LIDA is a library for generating data visualizations and data-faithful infographics.
-    LIDA is grammar agnostic (will work with any programming language and visualization
-    libraries e.g. matplotlib, seaborn, altair, d3 etc) and works with multiple large language
-    model providers (OpenAI, Azure OpenAI, PaLM, Cohere, Huggingface). Details on the components
-    of LIDA are described in the [paper here](https://arxiv.org/abs/2303.02927) and in this
-    tutorial [notebook](notebooks/tutorial.ipynb). See the project page [here](https://microsoft.github.io/lida/) for updates!.
-
-   This demo shows how to use the LIDA python api with Streamlit. [More](/about).
-
+    A Demo app for generating data visualizations and data-faithful infographics using various libraries,
+     such as Matplotlib, Seaborn, Altair, and D3. 
+     It supports integration with multiple large language model providers, 
+     including OpenAI, Azure OpenAI, PaLM, Cohere, and Hugging Face.
    ----
 """)
 
