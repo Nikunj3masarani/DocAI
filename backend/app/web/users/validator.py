20f471f59021e16f266868a5b8a8f25c5cb8262a
Nikunj Masrani <nikunj.masrani@smartsensesolutions.com>
2024-06-20 12:54:10 +0530

    - APIs to add users to the brain

--- backend/app/web/users/validator.py
+++ backend/app/web/users/validator.py
@@ -10,6 +10,10 @@ class InviteUserRequest(BaseModel):
     action: UserInviteAction
 
 
+class UserListRequest(BaseModel):
+    search: Optional[str] = None
+
+
 class Auth2Login(BaseModel):
     access_token: str
 
